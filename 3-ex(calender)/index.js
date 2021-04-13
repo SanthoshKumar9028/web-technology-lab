ea4424d773b4d3507cc380ff1a3d4bd1cd84238f
SanthoshKumar9028 <71515865+SanthoshKumar9028@users.noreply.github.com>
2021-04-13 21:55:20 +0530

    refactored the calender functions

--- 3-ex(calender)/index.js
+++ 3-ex(calender)/index.js
@@ -1,11 +1,13 @@
-function getTotalDays(year, month) {
+const getTotalDays = (() => {
   let days = [31, null, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  if (month === 1) {
-    if (isLeapYear(year)) return 29;
-    return 28;
-  }
-  return days[month];
-}
+  return (year, month) => {
+    if (month === 1) {
+      if (isLeapYear(year)) return 29;
+      return 28;
+    }
+    return days[month];
+  };
+})();
 
 function isLeapYear(year) {
   if (year % 400 === 0) return true;
@@ -25,29 +27,23 @@ function genMonthCalender(year, month) {
   let curDate = 1;
   //filling the first row
   while (weekDay < 7) {
-    cal[0][weekDay] = curDate;
-    weekDay++;
-    curDate++;
+    cal[0][weekDay++] = curDate++;
   }
   //filling row from second and fourth
   for (let i = 1; i < 4; i++) {
     for (let j = 0; j < 7; j++) {
-      cal[i][j] = curDate;
-      curDate++;
+      cal[i][j] = curDate++;
     }
   }
   //filling the last row
   for (let i = 0; i < 7; i++) {
     if (curDate > totalDays) break;
-    cal[4][i] = curDate;
-    curDate++;
+    cal[4][i] = curDate++;
   }
   //filling the remaing dates in first row
   let i = 0;
   while (curDate <= totalDays) {
-    cal[0][i] = curDate;
-    curDate++;
-    i++;
+    cal[0][i++] = curDate++;
   }
   return cal;
 }
